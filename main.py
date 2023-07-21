faa5c76895a4850f68b8a4a4f2c128314ad15116
villamont <villamont1578@gmail.com>
2023-07-20 18:11:41 -0600

    resultados.md

--- main.py
+++ main.py
@@ -3,18 +3,27 @@
 import api, ids
 import time
 import concurrent.futures
-import threading
+#import threading
 
-thread_local = threading.local()
+ruta='https://github.com/villamont/Laboratorio_2'
+#thread_local = threading.local()
 
 def read_users():
     for i in ids.ids:
         usr=api.getOneUser(i)
         print("Username is: ", usr["name"])
 
+def escritura_resultado(resul_time):
+# open file with mode 'w'
+    archivo = open(ruta + "log.txt", 'a+')
+    #print(resul, oper, time,file=archivo)
+    print("El resultado ejecutado sin threadind es:", resul_time,file=archivo)
+    archivo.close
+
 start_time = time.time()
 read_users()
-#download_all_sites(sites)
 duration = time.time() - start_time
+escritura_resultado(duration)
 print(f"Readed {len(ids.ids)} users in {duration} seconds")
+
         
--- threading_solution.py
+++ threading_solution.py
@@ -5,11 +5,22 @@ import time
 import concurrent.futures
 import threading
 
+ruta='https://github.com/villamont/Laboratorio_2/'
+
 thread_local = threading.local()
 
-def read_users(lista):
-    for i in lista:
-        usr=api.getOneUser(i)
+
+def escritura_resultado(resul_time):
+# open file with mode 'w'
+    archivo = open(ruta + "resultados.md", 'a+')
+    #print(resul, oper, time,file=archivo)
+    print("El resultado ejecutado con threadind es:", resul_time,file=archivo)
+    archivo.close
+
+
+def read_users(user):
+    #for i in lista:
+        usr=api.getOneUser(user)
         print("Username is: ", usr["name"])
 
 
@@ -23,5 +34,7 @@ if __name__ == "__main__":
 start_time = time.time()
 read_all_users(lista)
 duration = time.time() - start_time
+escritura_resultado(duration)
 print(f"Readed {len(lista)} users in {duration} seconds")
         
+
